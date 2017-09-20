c7c1d6b62c0197cbbc3476175e67ffb89a1cb558
Phuc Pham <ppham@csumb.edu>
2017-09-20 18:14:51 +0000

    Hw2 edited

--- Hw2/premade.php
+++ Hw2/premade.php
@@ -24,20 +24,23 @@
                 echo '<div id="tablecontent">';
                 echo '<form method="POST">';
                 
+                
+                echo '<table>';
                 echo '<tr id="table-header">';
                 echo '<td><strong>Dice Size</strong></td>';
                 echo '<td><strong>Roll!</strong></td>';
                 echo '</tr>';
+                
+                
                 for($i = 0; $i < count($diceArr); $i++)
                 {
                     $temp = $diceArr[$i];
-                    echo '<table>';
                     echo '<tr>';
                     echo '<td>'.$diceArr[$i].'</td>';
                     echo '<td><input type="submit" value="Roll!" name='.$temp.' /></td>';
                     echo '</tr>';
-                    echo '</table>';
                 }
+                echo '</table>';
                 echo '</form>';
                 
                 for($i = 0; $i < count($diceArr); $i++)
